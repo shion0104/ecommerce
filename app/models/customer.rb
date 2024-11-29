a7a980d9385774e24302546b510bce4f3e2260df
shion0104 <zhiyinzhongze@gmail.com>
2024-11-29 15:59:30 +0900

    Implementing Authentication Functions with devise

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,13 +2,12 @@ class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
-  
+         :recoverable, :rememberable, :validatable
+
   with_options presence: true do
     validates :name
     validates :status
   end
 
   enum :status, { normal: 0, withdrawn: 1, banned: 2 }
-
 end
