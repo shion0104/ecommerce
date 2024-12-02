e9d322525dd6d8806984888a97caf14f2f618d52
shion0104 <zhiyinzhongze@gmail.com>
2024-12-02 11:24:40 +0900

    コントローラーの作成

---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ApplicationRecord
+ with_options presence: true do
+  validates :name
+  validates :description
+  validates :price
+  validates :stock
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_29_030622) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_02_021602) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -38,5 +38,14 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_29_030622) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.integer "stock", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
